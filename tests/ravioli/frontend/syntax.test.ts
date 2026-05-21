fc1bb4865303a99e6493c3d41c3e3cedfa52d54f
Jimmy Pang <davnins2003@gmail.com>
2026-05-21 12:44:00 +0200

    refactor: update highlightSQL import path in syntax test

--- tests/ravioli/frontend/syntax.test.ts
+++ tests/ravioli/frontend/syntax.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from 'vitest';
 import { api } from '../../../src/ravioli/frontend/src/services/api';
-import { highlightSQL } from '../../../src/ravioli/frontend/src/components/Notebook';
+import { highlightSQL } from '../../../src/ravioli/frontend/src/components/notebook/utils';
 
 test('api service is syntactically valid and exportable', () => {
   expect(api).toBeDefined();
